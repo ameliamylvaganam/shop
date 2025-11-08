968f5b9ec8da57d9643bc0d269bbfd879bd6476d
Amelia Mylvaganam <amelia.mylvaganam@gmail.com>
2025-11-08 00:09:00 -0500

    shop has product cardsgit add .

--- data.js
+++ data.js
@@ -1,20 +1,36 @@
 
 let data = {
-    "main": "BALAY MEANS HOME.",
-    "about": "Balay means home.<br><br>We are a family-owned Vancouver-based company with over 20 years experience in residential property management. Our rental properties are all located in the West End. We provide comfortable accommodations and on-site management staff for our tenants.<br><br>If you are interested in making your home with us, please <a href='contact.html'>contact us</a>.",
-    "nav":[ "products", "one-offs", "about", "contact"],
-    "products":[
+    main: "BALAY MEANS HOME.",
+    about: "Balay means home.<br><br>We are a family-owned Vancouver-based company with over 20 years experience in residential property management. Our rental properties are all located in the West End. We provide comfortable accommodations and on-site management staff for our tenants.<br><br>If you are interested in making your home with us, please <a href='contact.html'>contact us</a>.",
+    nav:[ "all products", "one-offs", "about"],
+    products:[
         {
-            "title": "travelling interior zine",
-            "description": "20pg pocket road trip photozine with images and accompanying text. color-printed on textured paper. hand-bound.",
-            "image": "",
-            "price": 4,
+            slug: "travelling-interior",
+            title: "travelling interior zine",
+            description: "20pg pocket road trip photozine with images and accompanying text. color-printed on textured paper. hand-bound.",
+            coverimage: "travellinginteriorzine.jpg",
+            price: 4,
         },
         {
-            "title": "tops zine",
-            "description": "24pg pocket photozine with images taken on my screenless digital camera. color-printed on white paper with sticker detail. hand-bound.",
-            "image": "",
-            "price": 4,
+            slug: "tops",
+            title: "tops zine",
+            description: "24pg pocket photozine with images taken on my screenless digital camera. color-printed on white paper with sticker detail. hand-bound.",
+            coverimage: "topszine.png",
+            price: 4,
+        },
+         {
+            slug: "button-decor",
+            title: "button decorations",
+            description: "20pg pocket road trip photozine with images and accompanying text. color-printed on textured paper. hand-bound.",
+            coverimage: "travellinginteriorzine.jpg",
+            price: 4,
+        },
+         {
+            slug: "clover-ring",
+            title: "clover ring",
+            description: "stainless steel. size 6.",
+            coverimage: "topszine.png",
+            price: 4,
         },
         
     ]
